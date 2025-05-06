2c89cfe2ef92ef3a4cc65e4d8f497210c1688f26
Rodrigo Herce <935413@unizar.es>
2025-05-06 18:13:25 +0200

    MakeFile y mostrar tablero

--- mainGSenku.cpp
+++ mainGSenku.cpp
@@ -47,12 +47,25 @@ bool inicializarTablero(const string nombreFichero, tpTablero &tablero){
 // Pre: true 
 // Post: lee los movimientos válidos del fichero que se le pasa como argumento 
 //      inicializando la estructura y devolviendo true si todo ha ido bien y false si ha habido algún error
-bool inicializarMovimientosValidos(const string nombreFichero, tpMovimientosValidos &movimientos);
+//bool inicializarMovimientosValidos(const string nombreFichero, tpMovimientosValidos &movimientos);
 
 // Pre: tablero contiene el estado actual de la ejecución de la búsqueda de la solución
 // Post: Se ha mostrado el tablero por pantalla
 void mostrarTablero(const tpTablero & tablero){
     
+    for(int i = 0;i < tablero.nfils;i++){
+        for(int j = 0;j < tablero.ncols;j++){
+            if(tablero.matriz[i][j] == NO_USADA){
+                cout << "- ";
+            }else if(tablero.matriz[i][j] == VACIA){
+                cout << "x ";
+            }else{
+                cout << "o ";
+            }
+        }
+        cout << endl;
+    }
+
 }
 
 // Pre: tablero contiene el estado inicial del que se parte para la búsqueda, 
@@ -60,15 +73,18 @@ void mostrarTablero(const tpTablero & tablero){
 //      solucionParcial contiene la solución actual como lista de movimientos, En el tablero se han colocada las n primeras piezas de vEntrada, en la columnas indicadas respectivamente en vSalida
 // Post: solucionParcial contendrá la lista de movimientos completa (si no se llega a una solución, estará vacía, numMovs == 0)
 //       Devuelve 1 si encuentra solución, -1 si no la encuentra.
-int buscaSolucion(tpTablero &tablero, const tpMovimientosValidos &movValidos, tpListaMovimientos &solucionParcial, const int retardo=0);
+//int buscaSolucion(tpTablero &tablero, const tpMovimientosValidos &movValidos, tpListaMovimientos &solucionParcial, const int retardo=0);
 
 
 // Pre: listaMovimientos contiene la lista de movimientos con la solucion 
 // Post: escribe la lista de movimientos en el fichero que se le pasa como argumento siguiendo el 
 //      formato especificado en el guión (si está vacía, se escribe un -1 en el fichero)
-void escribeListaMovimientos (string nombreFichero, const tpListaMovimientos &solucion); 
+//void escribeListaMovimientos (string nombreFichero, const tpListaMovimientos &solucion); 
 
 
 int main(){
-
+    string nomTablero;
+    tpTablero tablero;
+    inicializarTablero(nomTablero, tablero);
+    mostrarTablero(tablero);
 }
